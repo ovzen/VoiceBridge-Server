8d4ebb1b8b1d5fd7e8e5165a5f195d22e8590232
ovzen <rueugen@mail.ru>
2026-03-22 21:59:02 +0700

    Сборка Windows, конфиг в AppData server_config.json

--- config_manager.py
+++ config_manager.py
@@ -1,7 +1,20 @@
 import json
 import os
+import sys
 
-CONFIG_FILE = "server_config.json"
+if sys.platform == 'win32':
+    appdata = os.getenv('APPDATA')
+    if appdata is None:
+        # fallback
+        appdata = os.path.expanduser('~\\AppData\\Roaming')
+    base_dir = os.path.join(appdata, 'VoiceBridge')
+else:
+    # Linux/macOS
+    base_dir = os.path.expanduser('~/.config/VoiceBridge')
+
+CONFIG_DIR = base_dir
+CONFIG_FILE = os.path.join(CONFIG_DIR, 'server_config.json')
+
 DEFAULT_CONFIG = {
     "host": "0.0.0.0",
     "server_ip": "192.168.0.17",
@@ -18,6 +31,8 @@ DEFAULT_CONFIG = {
 }
 
 def load_config():
+    os.makedirs(CONFIG_DIR, exist_ok=True)
+
     config = DEFAULT_CONFIG.copy()
     if os.path.exists(CONFIG_FILE):
         try:
@@ -26,10 +41,13 @@ def load_config():
                 config.update(loaded)
         except Exception as e:
             print(f"Ошибка загрузки конфигурации: {e}")
+    else:
+        save_config(config)
     return config
 
 def save_config(config):
     try:
+        os.makedirs(CONFIG_DIR, exist_ok=True)
         with open(CONFIG_FILE, "w", encoding="utf-8") as f:
             json.dump(config, f, indent=4)
     except Exception as e:
